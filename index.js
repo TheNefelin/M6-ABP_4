dedd287d55a72d96197cd408c3cb035025f7ea20
Nefelin <flcarmonac@yahoo.com>
2023-03-15 12:16:17 -0300

    Update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 // npm install express
 // npm install hbs
+// npm install body-parser
 // npm init
 
 import express from "express";
@@ -7,6 +8,8 @@ import hbs from "hbs";
 import rutas from "./routes/routes.js"
 import { fileURLToPath } from "url";
 import { dirname, join } from "path";
+import bodyParser from "body-parser"
+
 const _root = import.meta.url;
 const _dirname = dirname(fileURLToPath(_root));
 const app = express();
@@ -14,7 +17,7 @@ const app = express();
 app.listen(3000);
 app.set("view engine", "hbs");
 app.use(express.static("public"));
-app.use(rutas);
 hbs.registerPartials(join(_dirname, "/views/partials"));
-
-
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+app.use(rutas);
--- routes/routes.js
+++ routes/routes.js
@@ -1,43 +1,48 @@
 import {Router} from "express"
 import * as fs from "fs"
+
 const myRoutes = Router();
 
 myRoutes.get("/", (req, res) => {
     fs.readFile("./data/data.json", (err, data) => {
         if (err) throw err
         const dt =  JSON.parse(data);
-        console.log(dt);
         res.render("index", dt);
     });
 });
 
-// myRoutes.get("/", (req, res) => {
-//     fs.readFile("./data/data.json", (err, data) => {
-//         if (err) throw err
-//         const dt =  JSON.parse(data);
-//         res.render("index", dt);
-//     });
-// });
-
 myRoutes.get("/eliminar/:id", (req, res) => {
-    // const id = req.params['myId'];
-    // fs.readFile("./data/data.json", (err, data) => {
-    //     if (err) throw err
-    //     Promise.all([
-    //         ... Object.values(JSON.parse(data))[0]
-    //     ])
-    //     .then(dt => {
-    //         dt = {id: id, ... dt[id]}
-    //         console.log(dt)
-    //         res.render("eliminar");
-    //     });
-    // });
+    const id = req.params['id'];
+    
+    fs.readFile("./data/data.json", (err, data) => {
+        if (err) throw err
+        const dt =  JSON.parse(data);
+        dt.almuerzos.splice(id,1);
+        
+        fs.writeFile("./data/data.json", JSON.stringify(dt), err => {
+            if (err) throw err
+        });
 
-    res.render("eliminar");
+        res.render("index", dt);
+    });
 });
 
 myRoutes.get("/crear", (req, res) => {
     res.render("crear");
+});
+
+myRoutes.post("/crear", (req, res) => {
+    fs.readFile("./data/data.json", (err, data) => {
+        if (err) throw err
+        const dt =  JSON.parse(data);
+        dt.almuerzos.push({nombre: req.body.nombre, precio: req.body.precio});
+        
+        fs.writeFile("./data/data.json", JSON.stringify(dt), err => {
+            if (err) throw err
+        });
+
+        res.render("index", dt);
+    });
 });
 
 export default myRoutes;
